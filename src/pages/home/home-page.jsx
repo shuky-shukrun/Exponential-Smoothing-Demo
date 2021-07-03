beda090a88982cc54a736af0e57af2c830f357c4
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-07-03 13:36:38 +0300

    fix graph layout

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -7,7 +7,7 @@ const HomePage = (props) => {
     <div className={`${props.className ? props.className : ""}`}>
       <div className="row">
         <LeftMenu className="col-md-4 mx-3" />
-        <Card className="col-md-7 mx-3">
+        <Card className="align-self-start col-md-7 mx-3 ">
           <h5 className="card-title">Title!</h5>
           <p className="card-text">Some Text</p>
           <Graph />
