061ede68d1514f8fdc0120d73db5ebdbbb18de71
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2021-07-08 14:30:27 +0300

    fix navbar for mobile

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
